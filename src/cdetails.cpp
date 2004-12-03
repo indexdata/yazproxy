aa1ce475fe15391b9a3d4644495199fb59ad2ca6
Adam Dickmeiss <adam@indexdata.dk>
2004-12-03 15:50:53 +0000

    Added test program for the use of CDetails object from USEMARCON.

---
+++ src/cdetails.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <yaz/nmem.h>
+
+#if HAVE_USEMARCON
+#include <objectlist.h>
+
+CDetails *x(CDetails *p)
+{
+    return p;
+}
+#endif
+
+int main(int argc, char **argv)
+{
+#if HAVE_USEMARCON
+    CDetails local;
+    CDetails *ptr = new CDetails();
+
+    x(&local);
+    x(ptr);
+
+    delete ptr;
+#endif
+    exit(0);
+}
