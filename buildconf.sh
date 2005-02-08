96b3b41ac0deee62578f791afb46c9b5015f2962
Adam Dickmeiss <adam@indexdata.dk>
2005-02-08 14:06:51 +0000

    Use acloca -I. because of usemarcon.m4

--- buildconf.sh
+++ buildconf.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.4 2004-08-16 12:39:44 adam Exp $
+# $Id: buildconf.sh,v 1.5 2005-02-08 14:06:51 adam Exp $
 set -x
 dir=`aclocal --print-ac-dir`
-if [ -f $dir/yazpp.m4 ]; then
-	aclocal
-else
-	aclocal -I . 
-fi
+aclocal -I .
 libtoolize --force 
 automake -a 
 automake -a 
