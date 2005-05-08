2ae4531dae6f6d52f5284357061202bb680825ce
Adam Dickmeiss <adam@indexdata.dk>
2005-05-08 15:23:05 +0000

    Using more friendly buildconf from YAZ - slightly updated

--- buildconf.sh
+++ buildconf.sh
@@ -1,12 +1,67 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.5 2005-02-08 14:06:51 adam Exp $
+# $Id: buildconf.sh,v 1.6 2005-05-08 15:23:05 adam Exp $
 set -x
-dir=`aclocal --print-ac-dir`
 aclocal -I .
-libtoolize --force 
-automake -a 
-automake -a 
+libtoolize --automake --force 
+automake --add-missing 
+automake --add-missing 
 autoconf
+set -
 if [ -f config.cache ]; then
 	rm config.cache
+fi
+
+enable_configure=false
+enable_help=true
+sh_flags=""
+conf_flags=""
+case $1 in
+    -d)
+	sh_flags="-g -Wall"
+	enable_configure=true
+	enable_help=false
+	shift
+	;;
+    -c)
+	sh_flags=""
+	enable_configure=true
+	enable_help=false
+	shift
+	;;
+esac
+
+if $enable_configure; then
+    if test -n "$sh_flags"; then
+	CXXFLAGS="$sh_flags" ./configure $*
+    else
+	./configure $*
+    fi
+fi
+if $enable_help; then
+    cat <<EOF
+
+Build the Makefiles with the configure command.
+  ./configure [--someoption=somevalue ...]
+
+For help on options or configuring run
+  ./configure --help
+
+Build and install binaries with the usual
+  make
+  make check
+  make install
+
+Build distribution tarball with
+  make dist
+
+Verify distribution tarball with
+  make distcheck
+
+Or just build the Debian packages without configuring
+  dpkg-buildpackage -rfakeroot
+
+When building from a CVS checkout, you need these Debian tools:
+  docbook-utils, docbook, docbook-xml, docbook-dsssl, jade, jadetex,
+  libxslt1-dev, libyaz++-dev
+EOF
 fi
