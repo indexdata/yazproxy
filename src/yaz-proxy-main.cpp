82b0575e71269b113acb2bb7c7541a470ed07c9a
Adam Dickmeiss <adam@indexdata.dk>
2011-02-10 10:29:14 +0100

    Version 1.3.6

--- src/yaz-proxy-main.cpp
+++ src/yaz-proxy-main.cpp
@@ -1,5 +1,5 @@
 /* This file is part of YAZ proxy
-   Copyright (C) 1998-2009 Index Data
+   Copyright (C) 1998-2011 Index Data
 
 YAZ proxy is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
